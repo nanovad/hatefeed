8a95afde0a4e524977b0a6b6fb8ae1b96841319d
Nicholas 'nanovad' De Nova <16261742+nanovad@users.noreply.github.com>
2024-12-03 21:04:42 -0600

    Use build number when deploying
    Create the deployment archive with the build number in the filename to
    avoid overwriting previous versions when the build number gets bumped.

--- ui/deploy.sh
+++ ui/deploy.sh
@@ -6,9 +6,12 @@ flutter build web --release
 echo ">>> Done"
 
 # Parse the version number out of the json version manifest
-build_num=$(grep -Po '"version":.*?[^\\]"(?=,)' build/web/version.json | awk -F":" '{print $2}' | tr -d \")
+ver_num=$(grep -Po '"version":.*?[^\\]"(?=,)' build/web/version.json | awk -F":" '{print $2}' | tr -d \")
+build_num=$(grep -Po '"build_number".*?[^\\]"(?=,)' build/web/version.json | awk -F ":" '{print $2'} | tr -d \")
+echo "Version number: $ver_num"
 echo "Build number: $build_num"
-out_archive_name="hatefeed_ui_$build_num.xz"
+ver_and_build="${ver_num}_$build_num"
+out_archive_name="hatefeed_ui_$ver_and_build.xz"
 echo "Output archive name: $out_archive_name"
 out_archive_path="build/$out_archive_name"
 echo "Output archive path: $out_archive_path"
@@ -25,6 +28,6 @@ scp "$out_archive_path" "hatefeed.nanovad.com:ui_deploy/"
 
 read -p "Deploy into production? (Y/N): " deploy_prod
 if [[ $deploy_prod == [yY] ]]; then
-    ssh "hatefeed.nanovad.com" -t "HATEFEED_VERSION=\"$build_num\" bash deploy.sh"
+    ssh "hatefeed.nanovad.com" -t "HATEFEED_VERSION=\"$ver_and_build\" bash deploy.sh"
 fi
 echo "All done!"
