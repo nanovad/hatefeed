2d82db2e834e049d855740b9000b4935a26f2f59
Nicholas 'nanovad' De Nova <16261742+nanovad@users.noreply.github.com>
2025-06-22 18:58:51 -0500

    Target Flutter 3.32.4, upgrade dependencies

--- ui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,7 @@ import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
--- ui/test/processed_post_test.dart
+++ ui/test/processed_post_test.dart
@@ -1,5 +1,5 @@
 import 'package:hatefeed/processed_post.dart';
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test(
